15f67d1e4fa42f0cd8d8313ad995b8bb2498066d
jhs512 <jangka512@gmail.com>
2023-11-24 13:48:19 +0900

    step 2

---
+++ src/main/java/com/ll/sb20231114/domain/base/attr/entity/Attr.java
@@ -0,0 +1,10 @@
+package com.ll.sb20231114.domain.base.attr.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Attr {
+    @Id
+    private Long id;
+}
